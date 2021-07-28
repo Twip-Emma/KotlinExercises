8c53ddce8d3a9eddd86e6664b49b200c42580294
七画一只妖 <84392009+Twip-Emma@users.noreply.github.com>
2021-07-28 22:27:17 +0800

    Add files via upload

---
+++ Kotlin-Study/src/main/kotlin/001HelloWorld/App.kt
@@ -0,0 +1,5 @@
+package `001HelloWorld`
+
+fun main() {
+    println("Hello,World!")
+}
---
+++ Kotlin-Study/src/main/kotlin/002Function/Function.kt
@@ -0,0 +1,11 @@
+package `002Function`
+
+fun main() {
+
+}
+
+fun add1(a:Int,b:Int) :Int{
+    return a+b
+}
+
+fun sum2(a: Int, b: Int) = a + b
